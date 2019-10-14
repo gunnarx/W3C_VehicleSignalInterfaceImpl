ac66864be3b0e67a804715b2a569cd858ffff777
Gunnar Andersson <gandersson@genivi.org>
2019-10-14 14:21:27 +0200

    W3CServer.sh: Make it relocatable and set up GOPATH
    - Require that script is sourced.
    - Set up paths to allow script to be run when standing in another
    directory, and the script should still work.  Furthermore, the defined
    functions like starme should also still work, regardless of directory.
    - Add convenience function to modify GOPATH
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- server/Go/server-1.0/W3CServer.sh
+++ server/Go/server-1.0/W3CServer.sh
@@ -1,10 +1,48 @@
-#!/bin/bash  
+#!/bin/bash
+
+if [ "$0" != bash ] ; then
+  echo "This script should be *sourced* in your interactive shell (it is assuming bash)"
+  echo "Please run:  . $0 or source $0"
+  exit 1
+fi
+
+# Helper function
+add_gopath() {
+  local newpath="$1"
+  if [[ ! "$GOPATH" =~ (^|:)$newpath/?(:|$) ]]; then
+    echo "Adding $newpath to \$GOPATH"
+    if [ -z "$GOPATH" ] ; then
+      export GOPATH="$newpath"         # Was empty
+    else
+      export GOPATH="$GOPATH:$newpath" # Not empty, add to end
+    fi
+    echo "GOPATH is now : $GOPATH"
+  fi
+}
+
+# Logic to make this location independent, i.e. can be run
+# from any directory.
+
+# This thing gives us the path to this script, even if sourced...
+S=$(echo "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")
+D=$(dirname "$S")  # Capture the (relative) dir...
+# ... and get an absolute path out of that
+export W3C_SERVER_DIR="$(readlink -f "$D")"
+GIT_ROOT=$(readlink -f "$W3C_SERVER_DIR/../../..")
+echo "Project root is $GIT_ROOT"
+
+# The way http_mgr is written, it includes server-1.0/utils,
+# which must be then found relative to some part of $GOPATH
+# so therefore:
+add_gopath "$GIT_ROOT/server/Go"
+
+echo Setting up startme and stopme convenience functions
 
 startme() {
-    screen -d -m -S serverCore bash -c 'cd server-core  && go build && ./server-core'
-    screen -d -m -S serviceMgr bash -c 'go run service_mgr.go'
-    screen -d -m -S wsMgr bash -c 'go run ws_mgr.go'
-    screen -d -m -S httpMgr bash -c 'go run http_mgr.go'
+    screen -d -m -S serverCore bash -c "cd $W3C_SERVER_DIR/server-core && go build && ./server-core"
+    screen -d -m -S serviceMgr bash -c "cd $W3C_SERVER_DIR && go run service_mgr.go"
+    screen -d -m -S wsMgr bash -c "cd $W3C_SERVER_DIR && go run ws_mgr.go"
+    screen -d -m -S httpMgr bash -c "cd $W3C_SERVER_DIR && go run http_mgr.go"
 }
 
 stopme() {
