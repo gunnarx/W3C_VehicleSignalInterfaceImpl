9a8f4d28de041a6b7db3f92d3727f15b17e2777c
Gunnar Andersson <gandersson@genivi.org>
2019-10-14 14:24:28 +0200

    WIP Adds scripts for deps and build
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

---
+++ server/Go/server-1.0/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+MYDIR=$(dirname "$0")
+#cd "$MYDIR"
+GIT_ROOT=$(readlink -f "$MYDIR/../../..")
+echo "Project root is $GIT_ROOT"
+cd "$GIT_ROOT/server/Go/server-1.0/server-core"
+
+echo "Compiling server.  (If this was done already, there will be no output)"
+go build -v .
---
+++ server/Go/server-1.0/install_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+MYDIR=$(dirname "$0")
+#cd "$MYDIR"
+GIT_ROOT=$(readlink -f "$MYDIR/../../..")
+echo "Project root is $GIT_ROOT"
+cd "$GIT_ROOT"
+
+echo Getting packages
+echo
+echo "(NOTE - This will report errors for the local imports)"
+set +e
+go get -v ./...
+set -e
+# The above has some errors, return success anyway for now
+exit 0
