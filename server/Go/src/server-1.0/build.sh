bf3d0e61333b744c158b0388b55b66ce7e8cf869
Gunnar Andersson <gandersson@genivi.org>
2019-10-22 01:25:26 +0200

    WIP dependencies

--- server/Go/src/server-1.0/build.sh
+++ server/Go/src/server-1.0/build.sh
@@ -7,9 +7,12 @@ echo "Project root is $GIT_ROOT"
 cd "$GIT_ROOT/server/Go/src/server-1.0/server-core"
 
 # This is required to find several imports
-export GO_PATH="$GO_PATH:$GIT_ROOT/server/Go/src/server-1.0/"
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/"
 # This is required to find proto_files import
-export GO_PATH="$GO_PATH:$GIT_ROOT/server/Go/src/server-1.0/server-core"
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/server-core"
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/"
+
+echo "GOPATH: $GOPATH"
 
 echo "Compiling server.  (If this was done already, there will be no output)"
 go build -v .
--- server/Go/src/server-1.0/install_dependencies.sh
+++ server/Go/src/server-1.0/install_dependencies.sh
@@ -10,11 +10,17 @@ echo
 echo "(NOTE - This will report errors for the local imports)"
 set +e
 
-# This is required to find several imports
-export GO_PATH="$GO_PATH:$GIT_ROOT/server/Go/src/server-1.0/"
-# This is required to find proto_files import
-export GO_PATH="$GO_PATH:$GIT_ROOT/server/Go/src/server-1.0/server-core"
+echo "GOROOT: $GOROOT"
+unset GOROOT
 
+# This is required to find several imports
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/"
+# This is required to find proto_files import
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/server-core"
+
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/"
+
+echo "GOPATH: $GOPATH"
 # Fetch all dependencies
 go get -v ./...
 
