af647fc942b9dfc257a1dd688f735d81c0dea3d3
Gunnar Andersson <gandersson@genivi.org>
2019-10-22 02:02:20 +0200

    Remove server-1.0 subdirectory

--- server/Go/src/build.sh
+++ server/Go/src/build.sh
@@ -2,15 +2,14 @@
 
 MYDIR=$(dirname "$0")
 #cd "$MYDIR"
-GIT_ROOT=$(readlink -f "$MYDIR/../../../..")
+GIT_ROOT=$(readlink -f "$MYDIR/../../..")
 echo "Project root is $GIT_ROOT"
-cd "$GIT_ROOT/server/Go/src/server-1.0/server-core"
+cd "$GIT_ROOT/server/Go/src/server-core/src"
 
 # This is required to find several imports
-export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/"
-# This is required to find proto_files import
-export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/server-core"
 export GOPATH="$GOPATH:$GIT_ROOT/server/Go/"
+# This is required to find proto_files import
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-core"
 
 echo "GOPATH: $GOPATH"
 
--- server/Go/src/install_dependencies.sh
+++ server/Go/src/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 MYDIR=$(dirname "$0")
 #cd "$MYDIR"
-GIT_ROOT=$(readlink -f "$MYDIR/../../../..")
+GIT_ROOT="$(readlink -f "$MYDIR/../../..")"
 echo "Project root is $GIT_ROOT"
 cd "$GIT_ROOT"
 
@@ -14,16 +14,16 @@ echo "GOROOT: $GOROOT"
 unset GOROOT
 
 # This is required to find several imports
-export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/"
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/"
 # This is required to find proto_files import
-export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-1.0/server-core"
+export GOPATH="$GOPATH:$GIT_ROOT/server/Go/src/server-core"
 
 export GOPATH="$GOPATH:$GIT_ROOT/server/Go/"
 
 echo "GOPATH: $GOPATH"
 # Fetch all dependencies
 go get -v ./...
-
 set -e
+
 # The above has some errors, return success anyway for now
 exit 0
